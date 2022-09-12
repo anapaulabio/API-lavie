f930b471039c865f5a277c9c5b8f5d3ff709de60
Alyce Monteiro <mariamariamaria171@hotmail.com>
2022-09-12 20:41:35 -0300

    addind models

---
+++ src/models/psicologos.js
@@ -0,0 +1,32 @@
+const db = require("../database");
+const { DataTypes } = require("sequelize")
+
+const Psicologos = db.define(
+
+    "Psicologos", {
+
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nome: {
+            type: DataTypes.STRING,
+        },
+        email:  {
+            type: DataTypes.STRING,
+        },
+        senha: {
+            type: DataTypes.STRING,
+        },
+        apresentacao: {
+            type: DataTypes.TEXT,
+        },
+    },
+    {
+        tableName: "psicologos",
+        timestamps: false,
+    },
+);
+
+module.exports = Psicologos;
