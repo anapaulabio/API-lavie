1b1c16c40fcdeaa190058cb57376cd7a381ccf20
Alyce Monteiro <mariamariamaria171@hotmail.com>
2022-09-12 20:40:48 -0300

    adding server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { hasConection } = require('./database/config');
-const routes = require('./routes/routes');
+const { hasConection } = require('../src/database/config');
+//const routes = require('../src/routes/routes');
 
 const app = express();
 
 app.use(express.json());
 hasConection()
 
-app.use(routes)
+//app.use(routes)
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
