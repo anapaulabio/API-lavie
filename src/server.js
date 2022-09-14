4dd85e355db832d5e8991ff4217dd169241d13a2
Ana Paula <anapaulaoliveira.bio@outlook.com>
2022-09-14 17:33:59 -0300

    resolving conflicts

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
-<<<<<<< HEAD
-const { hasConection } = require('../src/database/config');
-const routes = require('../src/routes/routes');
-=======
 const { hasConection } = require('./database/config');
 const handleError = require ("./middlewares/handleError");
 const routes = require('./routes/routes');
->>>>>>> main
+
 
 const app = express();
 
@@ -14,10 +10,8 @@ app.use(express.json());
 hasConection()
 
 app.use(routes)
-<<<<<<< HEAD
-=======
 app.use(handleError);
->>>>>>> main
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
